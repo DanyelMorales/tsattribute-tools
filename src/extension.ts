2c99cf69933229efdf7e7ad2ae19a58570a2c249
Daniel Vera <dvera@sunset.com.mx>
2018-05-22 09:54:14 -0500

    Fix(Configuration): Fix user configuration
    Now can be loaded any time user changes her configuration.

--- src/extension.ts
+++ src/extension.ts
@@ -5,45 +5,62 @@ import { TypescriptCodeGen } from "./langs/Typescript";
 import { MomoThePug } from "./Definitions";
 
 const ConfigurationSectionName = "tsattribute-tools";
+const commandBaseName = ConfigurationSectionName + ".buildcode";
 
 /**
- * TODO: read targets from menu
- * TODO: deploy
+ * Plugin main entry point
  */
 export function activate(context: vscode.ExtensionContext) {
-    let gettersAndSetters = vscode.commands.registerCommand('tsattribute-tools.buildcode.getterAndSetter', () => {
-        let editor = vscode.window.activeTextEditor;
-        if (editor && isTypeScriptDocument(editor.document)) {
-            replaceCode(editor, TypescriptCodeGen.generator(createExtensionConfiguration()), [MomoThePug.Transformable.GETTER, MomoThePug.Transformable.SETTER, MomoThePug.Transformable.ATTRIBUTE]);
-        }
-    });
-    let getters = vscode.commands.registerCommand('tsattribute-tools.buildcode.getter', () => {
-        let editor = vscode.window.activeTextEditor;
-        if (editor && isTypeScriptDocument(editor.document)) {
-            replaceCode(editor, TypescriptCodeGen.generator(createExtensionConfiguration()), [MomoThePug.Transformable.GETTER, MomoThePug.Transformable.ATTRIBUTE]);
-        }
-    });
-    let setters = vscode.commands.registerCommand('tsattribute-tools.buildcode.setter', () => {
-        let editor = vscode.window.activeTextEditor;
-        if (editor && isTypeScriptDocument(editor.document)) {
-            replaceCode(editor, TypescriptCodeGen.generator(createExtensionConfiguration()), [MomoThePug.Transformable.SETTER, MomoThePug.Transformable.ATTRIBUTE]);
-        }
-    });
-    let attributes = vscode.commands.registerCommand('tsattribute-tools.buildcode.attributes', () => {
-        let editor = vscode.window.activeTextEditor;
-        if (editor && isTypeScriptDocument(editor.document)) {
-            replaceCode(editor, TypescriptCodeGen.generator(createExtensionConfiguration()), [MomoThePug.Transformable.ATTRIBUTE]);
-        }
-    });
-    context.subscriptions.push(attributes);
-    context.subscriptions.push(gettersAndSetters);
-    context.subscriptions.push(getters);
-    context.subscriptions.push(setters);
+    const editor = vscode.window.activeTextEditor;
+    if (editor && isTypeScriptDocument(editor.document)) {
+        registerLocalCommands(editor, context).forEach((subscription) => {
+            context.subscriptions.push(subscription);
+        });
+    }
 }
 
+/**
+ * Our commands will be registered here
+ * 
+ * @param editor 
+ * @param context 
+ */
+function registerLocalCommands(editor: vscode.TextEditor, context: vscode.ExtensionContext): any[] {
+    const gettersAndSetters = vscode.commands.registerCommand(commandBaseName + '.getterAndSetter', () => {
+        const config = createExtensionConfiguration();
+        replaceCode(editor, TypescriptCodeGen.generator(config), [
+            MomoThePug.Transformable.GETTER,
+            MomoThePug.Transformable.SETTER,
+            MomoThePug.Transformable.ATTRIBUTE
+        ]);
+    });
+    const getters = vscode.commands.registerCommand(commandBaseName + '.getter', () => {
+        const config = createExtensionConfiguration();
+        replaceCode(editor, TypescriptCodeGen.generator(config), [
+            MomoThePug.Transformable.GETTER,
+            MomoThePug.Transformable.ATTRIBUTE
+        ]);
+    });
+    const setters = vscode.commands.registerCommand(commandBaseName + '.setter', () => {
+        const config = createExtensionConfiguration();
+        replaceCode(editor, TypescriptCodeGen.generator(config), [
+            MomoThePug.Transformable.SETTER,
+            MomoThePug.Transformable.ATTRIBUTE
+        ]);
+    });
+    const attributes = vscode.commands.registerCommand(commandBaseName + '.attributes', () => {
+        const config = createExtensionConfiguration();
+        replaceCode(editor, TypescriptCodeGen.generator(config), [MomoThePug.Transformable.ATTRIBUTE]);
+    });
+    return [attributes, gettersAndSetters, getters, setters];
+}
+
+/**
+ * Get configuration from user workspace 
+ */
 function createExtensionConfiguration(): MomoThePug.ITranspilerConfiguration {
     const workspaceConfiguration = vscode.workspace.getConfiguration(ConfigurationSectionName);
-    const comments: boolean = workspaceConfiguration.get("tsattribute-tools.comments", false);
+    const comments: boolean = workspaceConfiguration.get<boolean>("comments", true);
     return {
         comments: comments
     };
@@ -61,6 +78,12 @@ function isTypeScriptDocument(document: vscode.TextDocument) {
     );
 }
 
+/**
+ * Generic code replacer for selected code to be transformed
+ * @param editor 
+ * @param strat 
+ * @param out 
+ */
 function replaceCode(editor: vscode.TextEditor, strat: MomoThePug.ILangCodeGen, out: MomoThePug.Transformable[]) {
     const selection = editor.selection;
     const text = editor.document.getText(selection);
